0803eccf7f8a674183f83fc7cd9716fc2d59e8dd
Alι Aryαɴ <85696485+AliAryanTech@users.noreply.github.com>
2023-04-16 11:41:42 +0500

    chore: update lyrics as API
    Weeb-API official doc's
    https://weeb-api.vercel.app/docs

--- src/Commands/Media/Lyrics.ts
+++ src/Commands/Media/Lyrics.ts
@@ -1,4 +1,3 @@
-import { Lyrics } from '../../lib'
 import { Message, Command, BaseCommand } from '../../Structures'
 import { IArgs } from '../../Types'
 
@@ -13,12 +12,12 @@ export default class extends BaseCommand {
     public override execute = async (M: Message, { context }: IArgs): Promise<void> => {
         if (!context) return void (await M.reply('Provide the name of the song to search the lyrics'))
         const term = context.trim()
-        const lyrics = new Lyrics()
-        const data = await lyrics.search(term)
+        const data = await this.client.utils.fetch<Lyrics[]>(`https://weeb-api.vercel.app/genius?query=${term}`)
         if (!data.length) return void (await M.reply(`Couldn't find any lyrics | *"${term}"*`))
         const buffer = await this.client.utils.getBuffer(data[0].image)
         let text = `🌿 *Title:* ${data[0].title} *(${data[0].fullTitle})*\n🍥 *Artist:* ${data[0].artist}`
-        text += `\n\n${await lyrics.parseLyrics(data[0].url)}`
+        const lyrics = await this.client.utils.fetch<string>(`https://weeb-api.vercel.app/lyrics?url=${data[0].url}`)
+        text += `\n\n${lyrics}`
         return void (await M.reply(buffer, 'image', undefined, undefined, text, undefined, {
             title: data[0].title,
             body: data[0].fullTitle,
@@ -28,4 +27,12 @@ export default class extends BaseCommand {
             mediaUrl: data[0].url
         }))
     }
+}
+
+interface Lyrics {
+    title: string
+    fullTitle: string
+    artist: string
+    image: string
+    url: string
 }
